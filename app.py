354acd5de3d95fddcf9f90daf5bc97c65cf3a66c
crasgaitis <crasgaitis@gmail.com>
2025-04-30 18:52:30 -0700

    update to benchmark eeg

--- app.py
+++ app.py
@@ -26,6 +26,65 @@ def do_max_rec():
     record(20, 'max_bandpowers.csv')
     return "You clicked the second button."
 
+def spiderman_go():
+    print("Running spiderman detection.")
+    record_live()
+    
+
+def record_live():
+    # Search for active LSL streams
+    print('Looking for an EEG stream...')
+    streams = resolve_byprop('type', 'EEG', timeout=2)
+    if len(streams) == 0:
+        raise RuntimeError('Can\'t find EEG stream.')
+    else:
+        print('Found it!')
+
+    # Set active EEG stream to inlet and apply time correction
+    print("Start acquiring data")
+    inlet = StreamInlet(streams[0], max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
+
+    # Get the stream info
+    info = inlet.info()
+    fs = int(info.nominal_srate())
+
+    # Initialize raw EEG data buffer
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    filter_state = None  # for use with the notch filter
+
+    # Compute the number of epochs in "buffer_length"
+    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
+                                SHIFT_LENGTH + 1))
+
+    while True:
+        # Obtain EEG data from the LSL stream
+        eeg_data, timestamp = inlet.pull_chunk(
+            timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+        if len(eeg_data) == 0:
+            continue
+
+        # Only keep the channel we're interested in
+        ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+
+        # Update EEG buffer with the new data
+        eeg_buffer, filter_state = update_buffer(
+            eeg_buffer, ch_data, notch=True,
+            filter_state=filter_state)
+
+        # Get newest samples from the buffer
+        data_epoch = get_last_data(eeg_buffer,
+                                   EPOCH_LENGTH * fs)
+
+        # Compute band powers
+        band_powers = compute_band_powers(data_epoch, fs)
+        delta, theta, alpha, beta = band_powers
+        
+        if beta > 0.5:
+            print('wow focus')
+        else:
+            print('no focus') 
+
 def record(duration_seconds=10, output_file='eeg_bandpowers.csv'):
     # Search for active LSL streams
     print('Looking for an EEG stream...')
@@ -104,6 +163,8 @@ def home():
             return do_min_rec()
         elif 'button2' in request.form:
             return do_max_rec()
+        elif 'button3' in request.form:
+            return spiderman_go()
     return render_template('index.html')
 
 if __name__ == '__main__':
